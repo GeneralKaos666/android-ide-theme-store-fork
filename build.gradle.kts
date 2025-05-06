78f399ccdb530b426ee90033e06637b32839b4b9
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-05-06 11:28:23 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,73 @@
-import org.gradle.api.Project
-import org.gradle.api.file.RegularFile
-import org.gradle.api.provider.Provider
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin) apply false
-    //id("kotlin-parcelize") apply false
+    alias(libs.plugins.versionsBenManes)
+    alias(libs.plugins.versionCatalogUpdate)
+    alias(libs.plugins.android.application) apply false // https://youtrack.jetbrains.com/issue/KT-31643/Unable-to-load-class-com.android.build.gradle.BaseExtension-with-Kotlin-plugin-applied-to-root-Gradle-project
+    alias(libs.plugins.android.library) apply false // see above url
+    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    //kotlin("kapt") version "2.0.20" apply false
-    alias(libs.plugins.hiltAndroid) apply false
-    alias(libs.plugins.kotlinAndroidKsp) apply false
-//    kotlin("jvm") version "2.0.20" apply false
-    alias(libs.plugins.version.catalog.update) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt.android) apply false // fun https://github.com/google/dagger/issues/3068
 }
 
-//apply(rootProject.layout.buildDirectory.file("../buildscripts/toml-updater-config.gradle"))
+//subprojects { parent!!.path.takeIf { it != rootProject.path }?.let { evaluationDependsOn(it)  } }
 
-apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+subprojects {
+    buildscript {
+        repositories {
+            google()
+            gradlePluginPortal()
+            mavenCentral()
+        }
+    }
+
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://androidx.dev/snapshots/builds/7913448/artifacts/repository") }
+    }
+}
+
+val dummyGoogleServices: Configuration by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = false
+
+    attributes {
+        attribute(Attribute.of("google.services.json", String::class.java), "dummy-json")
+    }
+}
+
+dependencies {
+    dummyGoogleServices(files(rootProject.file("build-config/dummy-data/dummy-google-services.json")))
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                when {
+                    isNonStable(candidate.version) && !isNonStable(currentVersion) -> {
+                        reject("Updating stable to non stable is not allowed")
+                    }
+                    candidate.module == "kotlin-gradle-plugin" && candidate.version != libs.versions.kotlin.get() -> {
+                        reject("Keep Kotlin version on the version specified in libs.versions.toml")
+                    }
+                    // KSP versions are compound versions, starting with the kotlin version
+                    candidate.group == "com.google.devtools.ksp" && !candidate.version.startsWith(libs.versions.kotlin.get()) -> {
+                        reject("KSP needs to stick to Kotlin version")
+                    }
+                }
+            }
+        }
+    }
+}

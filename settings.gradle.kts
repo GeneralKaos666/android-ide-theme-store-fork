0c85d69de9a6dccf524d7c7b509d7d74bcc06007
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-05-06 11:43:42 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,26 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            // Some plugins are not on the Gradle Plugins portal and require trickery to resolve
+            // since Maven repos know nothing of plugin IDs.
+            when (requested.id.id) {
+                "dagger.hilt.android.plugin" -> {
+                    useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
+                }
+                "com.google.firebase.crashlytics" -> {
+                    useModule("com.google.firebase.crashlytics:com.google.firebase.crashlytics.gradle.plugin:${requested.version}")
+                }
+                "shot" -> {
+                    useModule("com.karumi:shot:${requested.version}")
+                }
+            }
+        }
+    }
 }
 
+/*
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,6 +37,9 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+*/
 
 rootProject.name = "AndroidIDE Themes"
 include(":app")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
